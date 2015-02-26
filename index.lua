570cc2d7290d12b04e2a3f0eadf9ca8111a1aa98
Breno Ramalho Lemes <breno@br-lemes.net>
2015-02-26 18:09:14 -0400

    Tudo em um com sumário.

---
+++ index.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/haserl --shell=lua
+<% if ENV.SCRIPT_NAME then %>
+Content-type: text/html
+<% end %>
+
+<%
+require("lfs")
+
+function include(name, what)
+	file = io.open(string.format("%s/letra/%s", name, what))
+	if file then
+		print(file:read("*a"))
+		file:close()
+	end
+end
+
+%>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Hinário Adventista</title>
+</head>
+<body>
+	<% basepath = lfs.currentdir() %>
+	<h1 id="000">Sumário</h1>
+	<ul type="1">
+	<% for file in lfs.dir(".") do if file:match("^%d%d%d$") then %>
+		<li><a href="#<%= file %>"><% include(file, "nome") %></a></li>
+	<% end end %>
+	</ul>
+
+	<% for file in lfs.dir(".") do if file:match("^%d%d%d$") then %>
+		<h1 id="<%= file %>"><%= file %>. <% include(file, "nome") %></h1>
+		<% include(file, "letra") %>
+	<% end end %>	
+</body>
+</html>
