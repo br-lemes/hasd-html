43bc0589e136c853a73aa6b4e4457c4cb252db28
Breno Ramalho Lemes <breno@br-lemes.net>
2015-02-26 18:10:56 -0400

    Letra 1.

---
+++ 001/letra/index.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/haserl --shell=lua
+<% if ENV.SCRIPT_NAME then %>
+Content-type: text/html
+<% num = ENV.SCRIPT_NAME:match("/(%d%d%d)/") %>
+<% else num = ENV.PWD:match("/(%d%d%d)/") end %>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title><%in nome %> - Hinário Adventista</title>
+</head>
+<body>
+	<h1 id="<%= num %>"><%= num %>. <%in nome %></h1>
+	<%in letra %>
+</body>
+</html>
