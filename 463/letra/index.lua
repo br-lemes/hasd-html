d5fdf6ae823da33a317260c47f63c1bf9be12e7c
Breno Ramalho Lemes <breno@br-lemes.net>
2016-04-06 20:00:40 -0400

    Correção. Os arquivos devem ser symlink.

--- 463/letra/index.lua
+++ 463/letra/index.lua
@@ -1,17 +1,1 @@
-#!/usr/bin/haserl --shell=lua
-<% if ENV.SCRIPT_NAME then %>
-Content-type: text/html
-<% num = ENV.SCRIPT_NAME:match("/(%d%d%d)/") %>
-<% else num = ENV.PWD:match("/(%d%d%d)/") end %>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-	<meta charset="UTF-8">
-	<title>Hinário Adventista</title>
-</head>
-<body>
-	<h1 id="<%= num %>"><%= num %>. <%in nome %></h1>
-	<%in letra.html %>
-</body>
-</html>
+../../001/letra/index.lua
